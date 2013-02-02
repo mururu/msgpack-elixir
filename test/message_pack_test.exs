4c51c7ad2ec49045fac06579f97f802850fabccf
mururu <yuki@gnnk.net>
2013-02-02 20:27:52 +0900

    Implement

--- test/message_pack_test.exs
+++ test/message_pack_test.exs
@@ -3,7 +3,35 @@ Code.require_file "../test_helper.exs", __FILE__
 defmodule MessagePackTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert true
+  def check(term) do
+    assert term |> MessagePack.pack |> MessagePack.unpack == term
+  end
+
+  test "integer" do
+     [0, 1, 127, 128, 255, 256, 65535, 65536, 8388608, 0xFFFFFFFFFF, -1, -32, -33, -128, -129, -32768, -32769, 0xFFFFFFFFFF] |> Enum.each check(&1)
+  end
+
+  test "float" do
+    [0.0, 1.0, -1.0] |> Enum.each check(&1)
+  end
+
+  test "binary" do
+    ["", "hoge", "ほげ", <<"hoge">>, << "hoge", 2, 255, 1>>] |> Enum.each check(&1)
+  end
+
+  test "array" do
+    [[], :lists.seq(0, 16), :lists.seq(0, 0x10000), [[], nil]] |> Enum.each check(&1)
+  end
+
+  test "map" do
+    [{[]}, { :lists.seq(0, 0x10000) |> Enum.map(fn(i)-> {i, i*2} end) }] |> Enum.each check(&1)
+  end
+
+  test "boolean" do
+    [true, false] |> Enum.each check(&1)
+  end
+
+  test "nil" do
+    check(nil)
   end
 end
